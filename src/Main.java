8400c4ad83dc806149cc4b0227a1c944cf16072d
Brandon Castillo <tade.castillo.03@gmail.com>
2024-02-11 23:53:57 -0600

    Version 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,58 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     // se declara un ArrayList del tipo objeto Persona
-    private static ArrayList<Persona> personas = new ArrayList<>();
+    static ArrayList<Persona> personas = new ArrayList<>();
+    static File practica = new File("practica.txt");
+    static FileWriter escritor;
 
     public static void main(String[] args) {
         try {
             crearArchivo();
             mostrarArchivo(personas);
         } catch (Exception e) {
-            System.out.println(String.format("Excepcion %s", e));
+            System.out.printf("Excepcion %s%n", e);
+        }
+    }
+    // metodo que crea el archivo .txt, además aqui se ingresan los datos en el array list del objeto Persona
+    public static void crearArchivo() throws IOException {
+        if (practica.exists()) {
+            if (practica.length() == 0) {
+                escritor = new FileWriter(practica);
+                ingresarDatos();
+            } else {
+                escritor = new FileWriter(practica, true); // Abre el archivo en modo de añadir al final
+                Scanner sc = new Scanner(practica);
+                while (sc.hasNextLine()) {
+                    String[] reg = sc.nextLine().split(" ");
+                    personas.add(new Persona(reg[0], reg[1], reg[2], reg[3]));
+                }
+                sc.close();
+                mostrarArchivo(personas);
+                // Preguntar al usuario si quiere agregar más personas
+                sc = new Scanner(System.in);
+                System.out.println("¿Quieres registrar a otra persona? s/n ");
+                String s = sc.nextLine();
+                if (s.equalsIgnoreCase("s")) {
+                    ingresarDatos();
+                }
+            }
+        } else {
+            practica.createNewFile();
+            escritor = new FileWriter(practica);
+            ingresarDatos();
         }
     }
 
 
-    // metodo que crea el archivo .txt, además aqui se ingresan los datos en el array list del objeto Persona
-    public static void crearArchivo() throws IOException {
-        Scanner sc = new Scanner(System.in);
+    public static void ingresarDatos() throws IOException {
         boolean confirmar = false;
-        //Escritura del archivo
-        FileWriter escritor = new FileWriter("Practica1.txt");
-
-        while (confirmar != true){
+        Scanner sc = new Scanner(System.in);
+        while (!confirmar){
             System.out.println("Escribe un nombre");
             String nombre = sc.nextLine();
             System.out.println("Escribe apellido paterno");
@@ -52,21 +80,11 @@ public class Main {
 
     // metodo para que se muestre el archivo creado, el metodo lee el archivo, además le da un formato de "tabla"
     public static void mostrarArchivo(ArrayList<Persona> personas) throws FileNotFoundException {
-
-        Scanner sc = new Scanner(new File("Practica1.txt"));
-        System.out.println("Nombres en el archivo: ");
-        System.out.println(" "+"Nombre"+'\t'+'\t'+'\t'+"Apellido Paterno"+'\t'+'\t'+"Apellido Materno"+'\t'+'\t'+"Edad");
-
-        while (sc.hasNextLine()) { // <-- para confirmar si hay alguna otra linea en la entrada del escaner
-            String[] persona = sc.nextLine().split(" ");
-        }
-        sc.close();
-
+        System.out.println(" Nombres en el archivo ");
+        System.out.println(" "+"Nombre"+'\t'+"|"+'\t'+"Apellido Paterno"+'\t'+"|"+'\t'+"Apellido Materno"+'\t'+"|"+'\t'+"Edad");
         // Mostrar arrayList
         for (Persona persona: personas){
-            // System.out.println("");
             System.out.println(persona.toString());
-
         }
     }
 }
